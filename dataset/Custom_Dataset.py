1c1fdb2d1a7962fa4f3e9fc23a090bf7dbe1f100
EliasNoorzad <aliasnoorzad@hotmail.com>
2025-05-06 15:12:13 +0000

    Add Custom_Dataset.py

--- dataset/Custom_Dataset.py
+++ dataset/Custom_Dataset.py
@@ -9,14 +9,27 @@ import cv2, yaml
 class Custom_Dataset(Dataset):
   def __init__(self, targ_dir, split_txt):
 
+    """Create a custom Dataset by subclassing torch.utils.data.Dataset
+
+  Args:
+    targ_dir: path to 01 / 02 …
+    split_txt : path to train.txt  *or*  test.txt
+
+    Returns:
+      A dictionary of RGB, depth and their lables (R, t, bb) for each sample
+
+  """
+
     self.targ_dir = Path(targ_dir)
-  
+
     self.split_ids = []
 
+    # Reading split ids
     with open(split_txt, "r") as f:
       for line in f:
         self.split_ids.append(line.strip())
 
+    # Loading gt.yml
     with open(self.targ_dir / "gt.yml", "r") as f:
       gt_all = yaml.safe_load(f)
 
@@ -37,12 +50,11 @@ class Custom_Dataset(Dataset):
     depth_img = cv2.imread(str(depth_path), cv2.IMREAD_UNCHANGED)
 
     # Load the ground truth pose
-    pose_dict = self.gt_all[name][0]          
+    pose_dict = self.gt_all[name][0]
     R = np.array(pose_dict["cam_R_m2c"], dtype=np.float32).reshape(3,3)
     t = np.array(pose_dict["cam_t_m2c"], dtype=np.float32)
     bb= np.array(pose_dict["obj_bb"], dtype=np.int64)
 
-   # 6) package one sample --------------------------------
     sample = {
       "rgb"  : torch.from_numpy(rgb_img).permute(2,0,1).float() / 255.,
       "depth": torch.from_numpy(depth_img)[None].float(),
